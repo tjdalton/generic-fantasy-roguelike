1941f2f3ba55c5e59e34e60e3dd15ea2c52bfa16
the.othi@gmail.com <the.othi@gmail.com@424ad0a9-eec2-9229-a225-8aefdee7256d>
2012-04-11 04:05:14 +0000

    Added Logging class

---
+++ roguelike/roguelike/Log.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace roguelike
+{
+    class Log
+    {
+        private StreamWriter log;
+        public Log()
+        {
+            log = new StreamWriter("log.txt");
+        }
+
+        public void Error(String s)
+        {
+            log.Write("ERROR {0}", s);
+        }
+
+        public void Warning(String s)
+        {
+            log.Write("WARNING {0}", s);
+        }
+
+        public void Info(String s)
+        {
+            log.Write("INFO {0}", s);
+        }
+    }
+}
